6e80f2d3899ee4145a2051c12f4837854bee1793
dbarone <dbarone@factset.com>
2023-09-11 11:48:35 +0100

    fix: add try/except during reservation

--- robot/robin.py
+++ robot/robin.py
@@ -19,23 +19,27 @@ class Robin:
             seat_id, reserver_id = user_info.seat_id, user_info.reserver_id
             results[reserver_id] = {}
             for current in self.daterange(start, end):
-                if current.weekday() > 4:
-                    print(f"skip weekends {current.strftime('%Y-%m-%d')}")
-                    continue
+                try:
+                    if current.weekday() > 4:
+                        print(f"skip weekends {current.strftime('%Y-%m-%d')}")
+                        continue
 
-                print(
-                    f"make a reservation seat_id: {seat_id} date: {current.strftime(f'%Y-%m-%d')}")
-                reservation = Reservation(user_info, current)
-                if "-1" != reservation._get_id():
-                    continue
+                    print(
+                        f"make a reservation seat_id: {seat_id} date: {current.strftime(f'%Y-%m-%d')}")
+                    reservation = Reservation(user_info, current)
+                    if "-1" != reservation._get_id():
+                        continue
 
-                reservation_id = reservation._reserve()
-                if "-1" == reservation_id:
-                    print(f"reservation {reservation_id} too far in the future. stop going through dates")
-                    break
-                print(f"reservation successful with id {reservation_id}")
-                results[reserver_id][current.strftime("%Y-%m-%d")] = True
-
+                    reservation_id = reservation._reserve()
+                    if "-1" == reservation_id:
+                        print(f"reservation {reservation_id} too far in the future. stop going through dates")
+                        break
+                    print(f"reservation successful with id {reservation_id}")
+                    results[reserver_id][current.strftime("%Y-%m-%d")] = True
+                except AlreadyCheckedInError as e:
+                    print(f"error whilst reserving desk in: {e}")
+                    results[reserver_id][current.strftime("%Y-%m-%d")] = False
+                    pass
         return results
 
     def check_in(self, current: datetime) -> Dict:
